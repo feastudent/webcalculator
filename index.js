0c377274afa5f2ecab7b51a4ca4d15d570e41889
fealves <fealvesphone@gmail.com>
2021-03-08 06:47:47 -0500

    Code completed for the initial solution

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 
 const express = require('express');
 const app = express();
+const body = require('body-parser');
+app.use(body.urlencoded({extended: true})); //urlencoded is for parsing forms
 const PORT = process.env.PORT || 5000;
 
 
 app.get("/", (req,res) => {
-    res.send("Hello World!");
+    console.log("Serving: "+__dirname+"\\index.html");
+    res.sendFile(__dirname+"\\index.html");
 });
 
+app.post("/", (req,res) => {
+    console.log(req.body);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1+num2;
+    res.send("Thank you for posting that. ["+num1+" + "+num2+" = "+result+"]");
+});
 
 app.listen(PORT, () => console.log('Server is listening on port: '+PORT));
